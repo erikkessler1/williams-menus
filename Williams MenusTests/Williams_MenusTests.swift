6c4b3770c96c72bd60a25d7c28f96b2d30d94df2
Erik Kessker <erik.kessler1@gmail.com>
2015-03-06 09:29:12 -0500

    Initial Commit

---
+++ Williams MenusTests/Williams_MenusTests.swift
@@ -0,0 +1,36 @@
+//
+//  Williams_MenusTests.swift
+//  Williams MenusTests
+//
+//  Created by Erik Kessler on 3/6/15.
+//  Copyright (c) 2015 Erik Kessler. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Williams_MenusTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
